f05cabdb3f10b2197ee102d6f96bdb394be051f9
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-08-06 16:13:52 +0530

    commit permissions pipeline ios android functional

--- DocScannerPrototype/src/MainApp.js
+++ DocScannerPrototype/src/MainApp.js
@@ -5,18 +5,101 @@ import {NavigationContainer} from '@react-navigation/native';
 import {navigatorRef} from './rootNavigator';
 import MobileScanner from './containers/MobileScanner';
 import ScanContainer from './containers/ScanContainer';
-import {checkMultiple, PERMISSIONS} from 'react-native-permissions';
+import {checkMultiple, 
+        PERMISSIONS,
+        request} from 'react-native-permissions';
 
 MainStack = createDrawerNavigator();
 
 class MainApp extends Component{
     constructor(props){
         super(props);
+        this.permissionsAndroid = this.permissionsAndroid.bind(this);
+        this.getPermissions = this.getPermissions.bind(this);
+    }
+
+    permissionsAndroid(statuses){
+        console.log(statuses)
+        if(statuses[PERMISSIONS.ANDROID.CAMERA]=='denied'){
+            const rationale = {
+                title : 'Camera Permissions',
+                message : 'Please Grant Camera Permissions to be able to Scan',
+                buttonPositive : 'Allow',
+                buttonNegative : 'Deny',
+            };
+            request(
+                PERMISSIONS.ANDROID.CAMERA,
+                rationale).then(result=>console.log(result));
+        }
+        if(statuses[PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE]=='denied'){
+            const rationale = { 
+                title : 'Camera Permissions',
+                message : 'Please Grant Camera Permissions to be able to Scan',
+                buttonPositive : 'Allow',
+                buttonNegative : 'Deny',
+            };  
+            request(
+                PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+                rationale).then(result=>console.log(result));
+        }
+        if(statuses[PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE]=='denied'){
+            const rationale = { 
+                title : 'Camera Permissions',
+                message : 'Please Grant Camera Permissions to be able to Scan',
+                buttonPositive : 'Allow',
+                buttonNegative : 'Deny',
+            };      
+            console.log('asking');
+            request(
+                PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+                rationale).then(result=>console.log(result));
+        }  
+    }
+
+    permissionsIos(statuses){
+        if(statuses[PERMISSIONS.IOS.CAMERA]=='denied'){
+            const rationale = {
+                title : 'Camera Permissions',
+                message : 'Please Grant Camera Permissions to be able to Scan',
+                buttonPositive : 'Allow',
+                buttonNegative : 'Deny',
+            };
+            request(
+                PERMISSIONS.ANDROID.CAMERA,
+                rationale).then(result=>console.log(result));
+        }
+        if(statuses[PERMISSIONS.IOS.MEDIA_LIBRARY]=='denied'){
+            const rationale = {
+                title : 'Camera Permissions',
+                message : 'Please Grant Camera Permissions to be able to Scan',
+                buttonPositive : 'Allow',
+                buttonNegative : 'Deny',
+            };
+            console.log('asking');
+            request(
+                PERMISSIONS.IOS.MEDIA_LIBRARY,
+                rationale).then(result=>console.log(result));
+        }
+    }
+
+    
+    componentDidMount(){
+        this.getPermissions();
     }
 
     getPermissions(){
         if(Platform.OS == 'android'){
-            
+            checkMultiple([
+                PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+                PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+                PERMISSIONS.ANDROID.CAMERA,
+            ]).then(statuses=>this.permissionsAndroid(statuses));
+        }
+        else if(Platform.OS=='ios'){
+            checkMultiple([
+                PERMISSIONS.IOS.CAMERA,
+                PERMISSIONS.IOS.MEDIA_LIBRARY
+            ]).then(statuses=>this.permissionsIos(statuses));
         }
     }
 
