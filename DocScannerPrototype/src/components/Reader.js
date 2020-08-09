86404e1c663971ac0d97219a9af80b8039764fa8
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-08-09 16:34:03 +0530

    commit gesture handler for page change in reader

--- DocScannerPrototype/src/components/Reader.js
+++ DocScannerPrototype/src/components/Reader.js
@@ -4,6 +4,8 @@ import {StyleSheet,
         Dimensions,
         SafeAreaView,
         View,
+        PanResponder,
+        Animated,
         Text} from 'react-native';
 import Pdf from 'react-native-pdf';
 import Orientation from 'react-native-orientation-locker';
@@ -20,6 +22,38 @@ class Reader extends Component{
             width: this.props.width
         };
         this.pdf = null;
+        this.PanResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onPanResponderRelease: (evt, gestureState) => {
+                let x = gestureState.dx;
+                let y = gestureState.dy;
+                if (Math.abs(x) > Math.abs(y)) {
+                    if (x >= 0) {
+                        this.onSwipePerformed('right')
+                    }
+                    else {
+                        this.onSwipePerformed('left')
+                    }
+                }
+                else {
+                    if (y >= 0) {
+                        this.onSwipePerformed('down')
+                    }
+                    else {
+                        this.onSwipePerformed('up')
+                    }
+                }
+            }
+        })
+    }
+
+    onSwipePerformed(direction){
+        if(direction=='up'){
+           this.nextPage(); 
+        }
+        else if(direction == 'down'){
+            this.prePage();
+        }
     }
 
     _onOrientationDidChange = (orientation) => {
@@ -80,95 +114,44 @@ class Reader extends Component{
                         backgroundColor : 'white',
                     }
                 ]}>
-                <View 
-                    style={{flexDirection: 'row'}}>
-                    <TouchableHighlight 
-                        disabled={this.state.page === 1}
-                        style={this.state.page === 1 ? styles.btnDisable : styles.btn}
-                        onPress={() => this.prePage()}>
-                        <Text 
-                            style={styles.btnText}>{'-'}</Text>
-                    </TouchableHighlight>
-                    <View 
-                        style={styles.btnText}>
-                        <Text 
-                            style={styles.btnText}>Page</Text>
-                    </View>
-                    <TouchableHighlight 
-                        disabled={this.state.page === this.state.numberOfPages}
-                        style={this.state.page === this.state.numberOfPages ? styles.btnDisable : styles.btn}
-                        onPress={() => this.nextPage()}>
-                        <Text 
-                            style={styles.btnText}>{'+'}</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight 
-                        disabled={this.state.scale === 1}
-                        style={this.state.scale === 1 ? styles.btnDisable : styles.btn}
-                        onPress={() => this.zoomOut()}>
-                        <Text 
-                            style={styles.btnText}>{'-'}</Text>
-                    </TouchableHighlight>
-                    <View 
-                        style={styles.btnText}>
-                        <Text 
-                            style={styles.btnText}>Scale</Text>
-                    </View>
-                    <TouchableHighlight 
-                        disabled={this.state.scale >= 3}
-                        style={this.state.scale >= 3 ? styles.btnDisable : styles.btn}
-                        onPress={() => this.zoomIn()}>
-                        <Text 
-                            style={styles.btnText}>{'+'}</Text>
-                    </TouchableHighlight>
-                    <View 
-                        style={styles.btnText}>
-                        <Text 
-                            style={styles.btnText}>{'Horizontal:'}</Text>
-                    </View>
-                    <TouchableHighlight 
-                        style={styles.btn} 
-                        onPress={() => this.switchHorizontal()}>
-                        {!this.state.horizontal ? (<Text 
-                                                        style={styles.btnText}>{'false'}</Text>) : (
-                            <Text 
-                                style={styles.btnText}>{'true'}</Text>)}
-                    </TouchableHighlight>
-                </View>
-                <View  
-                    style={{flex:1,width: this.state.width}}>
-                    <Pdf ref={(pdf) => {
-                        this.pdf = pdf;
-                    }}
-                        source={this.props.source}
-                        scale={this.state.scale}
-                        horizontal={this.state.horizontal}
-                        onLoadComplete={(
-                            numberOfPages, 
-                            filePath,
-                            {
-                                width,
-                                height
-                            },
-                            tableContents
-                        ) => {
-                            this.setState({
-                                numberOfPages: numberOfPages 
-                            });
-                            console.log(`total page count: ${numberOfPages}`);
-                            console.log(tableContents);
+                <Animated.View
+                    {...this.PanResponder.panHandlers}>
+                    <View  
+                        style={{flex:1,width: this.state.width}}>
+                        <Pdf ref={(pdf) => {
+                            this.pdf = pdf;
                         }}
-                        onPageChanged={(page, numberOfPages) => {
-                            this.setState({
-                                page: page
-                            });
-                            console.log(`current page: ${page}`);
-                        }}
-                        onError={(error) => {
-                            console.log(error);
-                        }}
-                        style={{flex:1}}
-                        />
-                </View>
+                            source={this.props.source}
+                            scale={this.state.scale}
+                            horizontal={this.state.horizontal}
+                            onLoadComplete={(
+                                numberOfPages, 
+                                filePath,
+                                {
+                                    width,
+                                    height
+                                },
+                                tableContents
+                            ) => {
+                                this.setState({
+                                    numberOfPages: numberOfPages 
+                                }); 
+                                console.log(`total page count: ${numberOfPages}`);
+                                console.log(tableContents);
+                            }}
+                            onPageChanged={(page, numberOfPages) => {
+                                this.setState({
+                                    page: page
+                                });
+                                console.log(`current page: ${page}`);
+                            }}
+                            onError={(error) => {
+                                console.log(error);
+                            }}
+                            style={{flex:1}}
+                            />
+                    </View>
+                </Animated.View>
             </SafeAreaView>
         )
     }
